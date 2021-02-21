a7c4adf579952fd63ba7b4017b9d6191c614e449
ghsamm <ghsamm@gmail.com>
2021-02-21 10:46:55 +0100

    feat: add back button to book details page

--- src/pages/book-details.tsx
+++ src/pages/book-details.tsx
@@ -1,15 +1,29 @@
 import React, { FunctionComponent } from 'react'
 import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
+import { ArrowBack } from '@styled-icons/material/ArrowBack'
+
 import BaseCard from '../components/BaseCard'
 import BaseImage from '../components/BaseImage'
 import Detail from '../components/Details'
 import ErrorMessage from '../components/ErrorMessage'
+import Link from '../components/Link'
 import LoadingSpinner from '../components/LoadingSpinner'
 import useRequest from '../hooks/useRequest'
 import getBook from '../services/getBook'
 
 const S = {
+    BookDetailsPage: styled.div``,
+    Navigation: styled.div`
+        margin: 20px;
+        display: flex;
+    `,
+    Back: styled.div`
+        padding: 10px 15px;
+        &:hover {
+            background-color: rgba(0,0,0,0.01);
+        }
+    `,
     Container: styled.div`
         display: flex;
         justify-content: center;
@@ -50,18 +64,27 @@ const BookDetailsPage: FunctionComponent = () => {
         return <ErrorMessage message='Failed to load book details!' />
     }
 
-    return <S.Container>
-        <S.Card>
-            <S.Image link={book.cover} />
-        </S.Card>
-        <S.Content>
-            <S.Title>{book.title}</S.Title>
-            <S.Details>
-                <Detail label='Written by' content={book.author} />
-                <Detail label='ISBN' content={book.isbn} />
-            </S.Details>
-        </S.Content>
-    </S.Container>
+    return <S.BookDetailsPage>
+        <S.Navigation>
+            <Link to='/'>
+                <S.Back>
+                    <ArrowBack width={20} />
+                </S.Back>
+            </Link>
+        </S.Navigation>
+        <S.Container>
+            <S.Card>
+                <S.Image link={book.cover} />
+            </S.Card>
+            <S.Content>
+                <S.Title>{book.title}</S.Title>
+                <S.Details>
+                    <Detail label='Written by' content={book.author} />
+                    <Detail label='ISBN' content={book.isbn} />
+                </S.Details>
+            </S.Content>
+        </S.Container>
+    </S.BookDetailsPage>
 
 }
 
